a14b53848ba54c7594827225101c64eb26a98770
cheungww <1329105041@qq.com>
2020-02-17 00:35:47 +0800

    实现功能：餐馆食品列表、购物车、店铺详情页、登录注册搜索美食

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
-  <div>
-    <router-view />
-    <FooterGuide v-show="$route.meta.showFooter" />
+  <div id="app">
+    <router-view/>
+    <FooterGuide v-show="$route.meta.showFooter"/>
   </div>
 </template>
 
 <script>
-import FooterGuide from './components/FooterGuide/FooterGuide'
+  import {mapActions} from 'vuex'
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
 
-export default {
-  components: {
-    FooterGuide
+  export default {
+
+    mounted () {
+      // this.$store.dispatch('getAddress')
+      this.getAddress()
+      this.getUserInfo()
+    },
+
+    methods: {
+      ...mapActions(['getAddress', 'getUserInfo'])
+    },
+
+    components: {
+      FooterGuide
+    }
   }
-}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .app
+    width 100%
+    height 100%
+    background #f5f5f5
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
 /*
-入口js
-*/
+入口JS
+ */
 import Vue from 'vue'
+import {Button} from 'mint-ui'
+import VueLazyload from 'vue-lazyload'
 import App from './App.vue'
-
 import router from './router'
-// eslint-disable-next-line no-new
+import store from './store'
+
+import './mock/mockServer' // 加载mockServer即可
+import loading from './common/imgs/loading.gif'
+import './filters' // 加载过滤器
+
+// 注册全局组件标签
+Vue.component(Button.name, Button)  // <mt-button>
+Vue.use(VueLazyload, { // 内部自定义一个指令lazy
+  loading
+})
+
 new Vue({
   el: '#app',
   render: h => h(App),
-  router
+  router, //使用上vue-router
+  store, // 使用上vuex
 })
